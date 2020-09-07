d918dcd61ac968cfda486b56d44ae0845335b926
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-09-07 19:38:02 +0100

    fill UserService.Data.Provider.MsSql.Ef

--- src/UserService.Data.Provider.MsSql.Ef/Migrations/20200718150017_AddRelationWithCompanies.cs
+++ src/UserService.Data.Provider.MsSql.Ef/Migrations/20200718150017_AddRelationWithCompanies.cs
@@ -1,6 +1,50 @@
-﻿namespace UserService.Data.Provider.MsSql.Ef.Migrations
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace UserService.Data.Provider.MsSql.Ef.Migrations
 {
-    class AddRelationWithCompanies
+    public partial class AddRelationWithCompanies : Migration
     {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CompanyId",
+                table: "Positions",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Positions_CompanyId",
+                table: "Positions",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Positions_Companies_CompanyId",
+                table: "Positions",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Positions_Companies_CompanyId",
+                table: "Positions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Positions_CompanyId",
+                table: "Positions");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "CompanyId",
+                table: "Positions",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+        }
     }
 }
--- src/UserService.Data.Provider.MsSql.Ef/UserServiceDbContext.cs
+++ src/UserService.Data.Provider.MsSql.Ef/UserServiceDbContext.cs
@@ -1,10 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using UserService.Models.Db;
 
 namespace UserService.Data.Provider.MsSql.Ef
 {
-    class UserServiceDbContext
+    /// <summary>
+    /// A class that defines the tables and its properties in the database.
+    /// For this particular case, it defines the database for the UserService.
+    /// </summary>
+    public class UserServiceDbContext : DbContext
     {
+        public UserServiceDbContext(DbContextOptions<UserServiceDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DbUser> Users { get; set; }
+        public DbSet<DbAchievement> Achievements { get; set; }
+
+        // Fluent API is written here.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
