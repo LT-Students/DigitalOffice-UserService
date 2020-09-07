9a3c953d9a6e195173ced375428e19927ba226fd
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-09-07 16:25:30 +0100

    add UserService.Data.UnitTests

--- src/UserService.Data/UserRepository.cs
+++ src/UserService.Data/UserRepository.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace UserService.Data.Interfaces
+namespace UserService.Data
 {
-    class IUserServiceRepository
+    class UserRepository
     {
     }
 }
--- test/UserService.Data.UnitTests/UserRepositoryTests.cs
+++ test/UserService.Data.UnitTests/UserRepositoryTests.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace UserService.Data
+namespace UserService.Data.UnitTests
 {
-    class UserServiceRepository
+    class UserRepositoryTests
     {
     }
 }
