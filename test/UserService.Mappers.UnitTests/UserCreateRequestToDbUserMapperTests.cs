5c8bbc9bb3be7d773182cc04f115e3048dc924f1
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-09-09 10:48:51 +0100

    fill UserService.Mappers.UnitTests

--- test/UserService.Mappers.UnitTests/UserCreateRequestToDbUserMapperTests.cs
+++ test/UserService.Mappers.UnitTests/UserCreateRequestToDbUserMapperTests.cs
@@ -1,10 +1,69 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
+using UserService.Mappers.Interfaces;
+using UserService.Models.Db;
+using UserService.Models.Dto;
 
 namespace UserService.Mappers.UnitTests
 {
-    class UserCreateRequestToDbUserMapperTests
+    public class UserCreateRequestToDbUserMapperTests
     {
+        private IMapper<UserCreateRequest, DbUser> mapper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mapper = new UserMapper();
+        }
+
+        [Test]
+        public void ShouldReturnNewDbUserWhenDataCorrect()
+        {
+            var request = new UserCreateRequest
+            {
+                FirstName = "Example",
+                LastName = "Example",
+                MiddleName = "Example",
+                Email = "Example@gmail.com",
+                Status = "Example",
+                Password = "Password",
+                IsAdmin = false
+            };
+
+            var result = mapper.Map(request);
+
+            var user = new DbUser
+            {
+                Email = "Example@gmail.com",
+                FirstName = "Example",
+                LastName = "Example",
+                MiddleName = "Example",
+                Status = "Example",
+                PasswordHash = Encoding.UTF8.GetString(new SHA512Managed().ComputeHash(
+                    Encoding.UTF8.GetBytes(request.Password))),
+                AvatarFileId = null,
+                IsActive = true,
+                IsAdmin = false
+            };
+
+            Assert.AreEqual(user.Email, result.Email);
+            Assert.AreEqual(user.FirstName, result.FirstName);
+            Assert.AreEqual(user.LastName, result.LastName);
+            Assert.AreEqual(user.MiddleName, result.MiddleName);
+            Assert.AreEqual(user.Status, result.Status);
+            Assert.AreEqual(user.PasswordHash, result.PasswordHash);
+            Assert.AreEqual(user.AvatarFileId, result.AvatarFileId);
+            Assert.AreEqual(user.IsActive, result.IsActive);
+            Assert.AreEqual(user.IsAdmin, result.IsAdmin);
+        }
+
+        [Test]
+        public void ShouldThrowArgumentNullExceptionWhenRequestIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => mapper.Map(null));
+        }
     }
 }
