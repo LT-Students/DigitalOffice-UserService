f3bfda2794cac8cf1deccfb72e93f9a42c1f36a7
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-09-09 10:41:50 +0100

    fill UserService.Validation.UnitTests

--- UserService.Validation.UnitTests/GetUserByEmailValidatorTests.cs
+++ UserService.Validation.UnitTests/GetUserByEmailValidatorTests.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentValidation;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
 
 namespace UserService.Validation.UnitTests
 {
     class GetUserByEmailValidatorTests
     {
+        private IValidator<string> validator;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            validator = new GetUserByEmailValidator();
+        }
+
+        [Test]
+        public void ShouldThrowValidationExceptionWhenEmailIsEmpty()
+        {
+            validator.ShouldHaveValidationErrorFor(x => x, "");
+        }
+
+        [Test]
+        public void ShouldThrowValidationExceptionWhenEmailIsInvalid()
+        {
+            validator.ShouldHaveValidationErrorFor(x => x, "wrongEmail");
+        }
+
+        [Test]
+        public void ShouldThrowValidationExceptionWhenEmailTooLong()
+        {
+            var email = new string('a', 129) + "@gmail.com";
+            validator.ShouldHaveValidationErrorFor(x => x, email);
+        }
+
+        [Test]
+        public void ShouldValidateSuccessfullyWhenEmailIsValid()
+        {
+            var email = "example@gmail.com";
+            validator.ShouldNotHaveValidationErrorFor(x => x, email);
+        }
     }
 }
