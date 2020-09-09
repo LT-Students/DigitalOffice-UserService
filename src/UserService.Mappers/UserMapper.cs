abf63d69ff25ebfb05962df59fe421a23e3039f4
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-09-09 07:40:34 +0100

    fill UserService.Mappers

--- src/UserService.Mappers/UserMapper.cs
+++ src/UserService.Mappers/UserMapper.cs
@@ -1,10 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using UserService.Mappers.Interfaces;
+using UserService.Models.Db;
+using UserService.Models.Dto;
 
 namespace UserService.Mappers
 {
-    class UserMapper
+    /// <summary>
+    /// Represents mapper. Provides methods for converting an object of <see cref="DbUser"/> type into an object of <see cref="User"/> type according to some rule.
+    /// </summary>
+    public class UserMapper : IMapper<DbUser, User>, IMapper<UserCreateRequest, DbUser>, IMapper<EditUserRequest, DbUser>
     {
+        public User Map(DbUser dbUser)
+        {
+            if (dbUser == null)
+            {
+                throw new ArgumentNullException(nameof(dbUser));
+            }
+
+            return new User
+            {
+                Id = dbUser.Id,
+                Achievements = dbUser.AchievementsIds?.Select(dbUserAchievement => new Achievement
+                {
+                    Id = dbUserAchievement.Achievement.Id,
+                    Message = dbUserAchievement.Achievement.Message,
+                    PictureFileId = dbUserAchievement.Achievement.PictureFileId
+                }).ToList(),
+                AvatarId = dbUser.AvatarFileId,
+                CertificatesIds = dbUser.CertificatesFilesIds?.Select(x => x.CertificateId).ToList(),
+                Email = dbUser.Email,
+                FirstName = dbUser.FirstName,
+                LastName = dbUser.LastName,
+                MiddleName = dbUser.MiddleName,
+                Status = dbUser.Status,
+                IsAdmin = dbUser.IsAdmin
+            };
+        }
+
+        public DbUser Map(EditUserRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            return new DbUser
+            {
+                Id = request.Id,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                MiddleName = request.MiddleName,
+                Status = request.Status,
+                PasswordHash = Encoding.UTF8.GetString(new SHA512Managed().ComputeHash(
+                    Encoding.UTF8.GetBytes(request.Password))),
+                AvatarFileId = request.AvatarFileId,
+                IsActive = request.IsActive,
+                IsAdmin = request.IsAdmin
+            };
+        }
+
+        public DbUser Map(UserCreateRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            return new DbUser
+            {
+                Id = Guid.NewGuid(),
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                MiddleName = request.MiddleName,
+                Status = request.Status,
+                PasswordHash = Encoding.UTF8.GetString(new SHA512Managed().ComputeHash(
+                    Encoding.UTF8.GetBytes(request.Password))),
+                AvatarFileId = null,
+                IsActive = true,
+                IsAdmin = request.IsAdmin
+            };
+        }
     }
 }
