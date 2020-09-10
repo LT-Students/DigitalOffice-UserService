de41761d5768cf83bf46c481d2f3da20a79f03e8
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-09-10 15:35:26 +0100

    fill UserService.Business.UnitTests

--- test/UserService.Business.UnitTests/DisablingUserByIdCommandTests.cs
+++ test/UserService.Business.UnitTests/DisablingUserByIdCommandTests.cs
@@ -1,10 +1,188 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using LT.DigitalOffice.Kernel.AccessValidator.Interfaces;
+using LT.DigitalOffice.UserService.Business.Interfaces;
+using LT.DigitalOffice.UserService.Data.Interfaces;
+using LT.DigitalOffice.UserService.Models.Db;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.UserService.Business.UnitTests
 {
-    class DisablingUserByIdCommandTests
+    public class DisablingUserByIdCommandTests
     {
+        #region Variables declaration
+        private Mock<IUserRepository> repositoryMock;
+        private Mock<IAccessValidator> accessValidatorMock;
+
+        private IDisableUserByIdCommand command;
+
+        private Guid userId;
+        private DbUser newDbUser;
+        private Guid requestingUserId;
+        #endregion
+
+        #region Setup
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            accessValidatorMock = new Mock<IAccessValidator>();
+            userId = Guid.NewGuid();
+            requestingUserId = Guid.NewGuid();
+
+            newDbUser = new DbUser
+            {
+                Id = userId,
+                Email = "Example",
+                FirstName = "Example1",
+                LastName = "Example1",
+                MiddleName = "Example1",
+                Status = "normal",
+                PasswordHash = "Example1",
+                AvatarFileId = Guid.NewGuid(),
+                IsActive = true,
+                IsAdmin = false
+            };
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            repositoryMock = new Mock<IUserRepository>();
+            command = new DisableUserByIdCommand(repositoryMock.Object, accessValidatorMock.Object);
+        }
+        #endregion
+
+        [Test]
+        public void ShouldThrowExceptionWhenUserIdNotFoundInDb()
+        {
+            accessValidatorMock
+                .Setup(x => x.IsAdmin())
+                .Returns(Task.FromResult(true));
+
+            accessValidatorMock
+                .Setup(x => x.HasRights(It.IsAny<int>()))
+                .Returns(Task.FromResult(true));
+
+            repositoryMock
+                .Setup(x => x.GetUserInfoById(userId))
+                .Throws(new Exception());
+
+            Assert.ThrowsAsync<Exception>(() => command.ExecuteAsync(userId, requestingUserId));
+            repositoryMock.Verify(repository => repository.GetUserInfoById(userId), Times.Once);
+            repositoryMock.Verify(repository => repository.EditUser(It.IsAny<DbUser>()), Times.Never);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenEditingUserInDb()
+        {
+            accessValidatorMock
+                .Setup(x => x.IsAdmin())
+                .Returns(Task.FromResult(true));
+
+            accessValidatorMock
+                .Setup(x => x.HasRights(It.IsAny<int>()))
+                .Returns(Task.FromResult(true));
+
+            repositoryMock
+                .Setup(x => x.GetUserInfoById(userId))
+                .Returns(newDbUser);
+
+            repositoryMock
+                .Setup(x => x.EditUser(It.IsAny<DbUser>()))
+                .Throws(new Exception());
+
+            Assert.ThrowsAsync<Exception>(() => command.ExecuteAsync(userId, requestingUserId));
+            repositoryMock.Verify(repository => repository.GetUserInfoById(userId), Times.Once);
+            repositoryMock.Verify(repository => repository.EditUser(It.IsAny<DbUser>()), Times.Once);
+        }
+
+        [Test]
+        public void ShouldUserDisabledSuccess()
+        {
+            accessValidatorMock
+                .Setup(x => x.IsAdmin())
+                .Returns(Task.FromResult(true));
+
+            accessValidatorMock
+                .Setup(x => x.HasRights(It.IsAny<int>()))
+                .Returns(Task.FromResult(true));
+
+            repositoryMock
+                .Setup(x => x.GetUserInfoById(userId))
+                .Returns(newDbUser);
+
+            repositoryMock
+                .Setup(x => x.EditUser(It.IsAny<DbUser>()))
+                .Returns(true);
+
+            command.ExecuteAsync(userId, requestingUserId);
+
+            repositoryMock.Verify(repository => repository.GetUserInfoById(userId), Times.Once);
+            repositoryMock.Verify(repository => repository.EditUser(It.IsAny<DbUser>()), Times.Once);
+        }
+
+        [Test]
+        public void ShouldUserDisabledSuccessWhenUserIsAdmin()
+        {
+            accessValidatorMock
+                .Setup(x => x.IsAdmin())
+                .Returns(Task.FromResult(true));
+
+            repositoryMock
+                .Setup(x => x.GetUserInfoById(userId))
+                .Returns(newDbUser);
+
+            repositoryMock
+                .Setup(x => x.EditUser(It.IsAny<DbUser>()))
+                .Returns(true);
+
+            command.ExecuteAsync(userId, requestingUserId);
+
+            repositoryMock.Verify(repository => repository.GetUserInfoById(userId), Times.Once);
+            repositoryMock.Verify(repository => repository.EditUser(It.IsAny<DbUser>()), Times.Once);
+        }
+
+        [Test]
+        public void ShouldUserDisabledSuccessWhenUserIsNotAdminAndHasRights()
+        {
+            accessValidatorMock
+                .Setup(x => x.IsAdmin())
+                .Returns(Task.FromResult(false));
+
+            accessValidatorMock
+                .Setup(x => x.HasRights(It.IsAny<int>()))
+                .Returns(Task.FromResult(true));
+
+            repositoryMock
+                .Setup(x => x.GetUserInfoById(userId))
+                .Returns(newDbUser);
+
+            repositoryMock
+                .Setup(x => x.EditUser(It.IsAny<DbUser>()))
+                .Returns(true);
+
+            command.ExecuteAsync(userId, requestingUserId);
+
+            repositoryMock.Verify(repository => repository.GetUserInfoById(userId), Times.Once);
+            repositoryMock.Verify(repository => repository.EditUser(It.IsAny<DbUser>()), Times.Once);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenUserHasNotEnoughRights()
+        {
+            accessValidatorMock
+                .Setup(x => x.IsAdmin())
+                .Returns(Task.FromResult(false));
+
+            accessValidatorMock
+                .Setup(x => x.HasRights(It.IsAny<int>()))
+                .Returns(Task.FromResult(false));
+
+            Assert.That(() => command.ExecuteAsync(userId, requestingUserId),
+                Throws.InstanceOf<Exception>().And.Message.EqualTo("Not enough rights."));
+            repositoryMock.Verify(repository => repository.GetUserInfoById(userId), Times.Never);
+            repositoryMock.Verify(repository => repository.EditUser(It.IsAny<DbUser>()), Times.Never);
+        }
     }
 }
--- test/UserService.Business.UnitTests/UserCreateCommandTests.cs
+++ test/UserService.Business.UnitTests/UserCreateCommandTests.cs
@@ -1,10 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentValidation;
+using LT.DigitalOffice.UserService.Business.Interfaces;
+using LT.DigitalOffice.UserService.Data.Interfaces;
+using LT.DigitalOffice.UserService.Mappers.Interfaces;
+using LT.DigitalOffice.UserService.Models.Db;
+using LT.DigitalOffice.UserService.Models.Dto;
+using Moq;
+using NUnit.Framework;
+using System;
 
 namespace LT.DigitalOffice.UserService.Business.UnitTests
 {
-    class UserCreateCommandTests
+    public class UserCreateCommandTests
     {
+        private Mock<IUserRepository> repositoryMock;
+        private IUserCreateCommand command;
+        private Mock<IValidator<UserCreateRequest>> validatorMock;
+        private Mock<IMapper<UserCreateRequest, DbUser>> mapperMock;
+        private Guid userId = Guid.NewGuid();
+
+        private UserCreateRequest request = new UserCreateRequest
+        {
+            FirstName = "Example",
+            LastName = "Example",
+            MiddleName = "Example",
+            Email = "Example@gmail.com",
+            Status = "Example",
+            Password = "Example",
+            IsAdmin = false
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            repositoryMock = new Mock<IUserRepository>();
+            mapperMock = new Mock<IMapper<UserCreateRequest, DbUser>>();
+            validatorMock = new Mock<IValidator<UserCreateRequest>>();
+
+            command = new UserCreateCommand(validatorMock.Object, repositoryMock.Object, mapperMock.Object);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenRepositoryThrowsException()
+        {
+            validatorMock.Setup(validator => validator.Validate(It.IsAny<IValidationContext>()).IsValid)
+                .Returns(true);
+            repositoryMock
+                .Setup(x => x.UserCreate(It.IsAny<DbUser>()))
+                .Throws(new Exception());
+
+            Assert.Throws<Exception>(() => command.Execute(request));
+        }
+
+        [Test]
+        public void ShouldCreateUserWhenUserDataIsValid()
+        {
+            validatorMock.Setup(validator => validator.Validate(It.IsAny<IValidationContext>()).IsValid)
+                .Returns(true);
+            repositoryMock
+                .Setup(x => x.UserCreate(It.IsAny<DbUser>()))
+                .Returns(userId)
+                .Verifiable();
+            mapperMock
+                .Setup(mapper => mapper.Map(It.IsAny<UserCreateRequest>()))
+                .Returns(new DbUser())
+                .Verifiable();
+
+            Assert.That(command.Execute(request), Is.EqualTo(userId));
+            mapperMock.Verify();
+            validatorMock.Verify(validator => validator.Validate(It.IsAny<IValidationContext>()), Times.Once);
+            repositoryMock.Verify();
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenMapperThrowsException()
+        {
+            validatorMock.Setup(validator => validator.Validate(It.IsAny<IValidationContext>()).IsValid)
+                .Returns(true);
+            repositoryMock
+                .Setup(x => x.UserCreate(It.IsAny<DbUser>()))
+                .Returns(userId);
+            mapperMock.Setup(mapper => mapper.Map(It.IsAny<UserCreateRequest>())).Throws<Exception>().Verifiable();
+
+            Assert.Throws<Exception>(() => command.Execute(request));
+            validatorMock.Verify(validator => validator.Validate(It.IsAny<IValidationContext>()), Times.Once);
+            mapperMock.Verify();
+            repositoryMock.Verify(repository => repository.UserCreate(It.IsAny<DbUser>()), Times.Never);
+        }
+
+        /*[Test]
+        public void ShouldThrowExceptionWhenValidatorThrowsException()
+        {
+            validatorMock.Setup(validator => validator.Validate(It.IsAny<IValidationContext>()).IsValid)
+                .Returns(false);
+            repositoryMock
+                .Setup(x => x.UserCreate(It.IsAny<DbUser>()))
+                .Returns(userId);
+            mapperMock
+                .Setup(mapper => mapper.Map(It.IsAny<UserCreateRequest>()))
+                .Returns(new DbUser());
+
+            Assert.Throws<ValidationException>(() => command.Execute(request));
+            validatorMock.Verify(validator => validator.Validate(It.IsAny<IValidationContext>()), Times.Once);
+            repositoryMock.Verify(repository => repository.UserCreate(It.IsAny<DbUser>()), Times.Never);
+            mapperMock.Verify(mapper => mapper.Map(It.IsAny<UserCreateRequest>()), Times.Never);
+        }*/
     }
 }
