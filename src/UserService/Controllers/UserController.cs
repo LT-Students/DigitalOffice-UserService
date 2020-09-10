39bf10a23e59c45baec0fb9432a18951fbb16a26
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-09-10 17:33:46 +0100

    configure servces

--- src/UserService/Controllers/UserController.cs
+++ src/UserService/Controllers/UserController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LT.DigitalOffice.UserService.Business.Interfaces;
+using LT.DigitalOffice.UserService.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.UserService.Controllers
 {
@@ -6,6 +10,35 @@ namespace LT.DigitalOffice.UserService.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        //TODO: Add all methods
+        [HttpGet("getUserById")]
+        public User GetUserById([FromServices] IGetUserByIdCommand getUserInfoByIdCommand, [FromQuery] Guid userId)
+            => getUserInfoByIdCommand.Execute(userId);
+
+        [HttpPost("register")]
+        public Guid CreateUser([FromServices] IUserCreateCommand command, [FromBody] UserCreateRequest request)
+        {
+            return command.Execute(request);
+        }
+
+        [HttpPost("editUser")]
+        public bool EditUser([FromServices] IEditUserCommand command, [FromBody] EditUserRequest request)
+        {
+            return command.Execute(request);
+        }
+
+        [HttpGet("getUserByEmail")]
+        public User GetUserByEmail([FromServices] IGetUserByEmailCommand command, [FromQuery] string userEmail)
+        {
+            return command.Execute(userEmail);
+        }
+
+        [HttpDelete("disableUserById")]
+        public async Task DisableUserById(
+            [FromServices] IDisableUserByIdCommand command,
+            [FromQuery] Guid userId,
+            [FromHeader] Guid requestingUser)
+        {
+            await command.ExecuteAsync(userId, requestingUser);
+        }
     }
 }
