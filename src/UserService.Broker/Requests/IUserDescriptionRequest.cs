c2ffdacfd139cf2257725b36c8e07c3ccfcd1070
djino <malkinevgeniy437@gmail.com>
2020-09-29 12:38:46 +0300

    broker logic

---
+++ src/UserService.Broker/Requests/IUserDescriptionRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LT.DigitalOffice.UserService.Broker.Requests
+{
+    public interface IUserDescriptionRequest
+    {
+        Guid GeneratedId { get; }
+        string Email { get; }
+        string FirstName { get; }
+        string LastName { get; }
+        string MiddleName { get; }
+    }
+}
