48b76ae52f23165e4461a4dc946b5b21e53265a1
lalag <lalagvanan@gmail.com>
2020-10-11 12:40:55 +0300

    Renamed file

--- test/UserService.Mappers.UnitTests/UserCredentialsMapperTests.cs
+++ test/UserService.Mappers.UnitTests/UserCredentialsMapperTests.cs
@@ -9,7 +9,7 @@ using System.Text;
 
 namespace LT.DigitalOffice.UserService.Mappers.UnitTests
 {
-    class UserCedentialsMapperTests
+    class UserCredentialsMapperTests
     {
         private IMapper<UserRequest, DbUserCredentials> mapperGetUser;
         private IMapper<UserRequest, DbUserCredentials> mapperEditUser;
