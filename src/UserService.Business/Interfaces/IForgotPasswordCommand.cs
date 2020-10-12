0ad39630c6bc526d13ce07c04759de6dcd7e2d35
djino <malkinevgeniy437@gmail.com>
2020-10-12 16:43:51 +0300

    added dots

--- src/UserService.Business/Interfaces/IForgotPasswordCommand.cs
+++ src/UserService.Business/Interfaces/IForgotPasswordCommand.cs
@@ -8,10 +8,10 @@ namespace LT.DigitalOffice.UserService.Business.Interfaces
     public interface IForgotPasswordCommand
     {
         /// <summary>
-        /// Method for getting user description by email and sent request in Message Service
+        /// Method for getting user description by email and sent request in Message Service.
         /// </summary>
-        /// <param name="userEmail">Specific user email</param>
-        /// <returns>Boolean operation result</returns>
+        /// <param name="userEmail">Specific user email.</param>
+        /// <returns>Boolean operation result.</returns>
         bool Execute(string userEmail);
     }
 }
