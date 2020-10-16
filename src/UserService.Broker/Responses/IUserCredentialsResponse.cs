e5d564cfa090d2beba64ca8fb061ffca8317887d
Evgeniy <malkinevgeniy437@gmail.com>
2020-10-16 12:35:56 +0300

    added new field for broker response (#14)

--- src/UserService.Broker/Responses/IUserCredentialsResponse.cs
+++ src/UserService.Broker/Responses/IUserCredentialsResponse.cs
@@ -9,5 +9,18 @@ namespace LT.DigitalOffice.Broker.Responses
     {
         Guid UserId { get; }
         string PasswordHash { get; }
+        string Salt { get; }
+        string UserLogin { get; }
+
+        static object CreateObj(Guid userId, string passwordHash, string salt, string userLogin)
+        {
+            return new
+            {
+                UserId = userId,
+                PasswordHash = passwordHash,
+                Salt = salt,
+                UserLogin = userLogin
+            };
+        }
     }
 }
