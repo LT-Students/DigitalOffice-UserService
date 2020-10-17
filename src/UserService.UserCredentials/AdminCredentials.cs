326ba409b7e92cbb16eb42544ab296447788fa67
djino <malkinevgeniy437@gmail.com>
2020-10-17 19:38:24 +0300

    Added new project
    New project for generate user hash and admin credentials storage

---
+++ src/UserService.UserCredentials/AdminCredentials.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("LT.DigitalOffice.UserService.Data.Provider.MsSql.Ef"),
+    InternalsVisibleTo("LT.DigitalOffice.UserService.Mappers.UnitTests")]
+
+namespace LT.DigitalOffice.UserService.UserCredentials.Admin
+{
+    internal static class AdminCredentials
+    {
+        private const string PASSWORD = "admin";
+
+        internal const string LOGIN = "admin";
+        internal const string EMAIL = "admin@lanit-tercom.com";
+        internal const string FIRST_NAME = "admin@lt";
+        internal const string LAST_NAME = "admin@lt";
+
+        internal static string salt = $"{Guid.NewGuid()}{Guid.NewGuid()}";
+
+        internal static string GetPasswordHash()
+        {
+            return UserPassword.GetPasswordHash(LOGIN, salt, PASSWORD);
+        }
+    }
+}
