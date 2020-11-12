20aac4daeb40ea773f5aea34a23cf89e7ed23362
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2020-11-12 15:12:31 +0300

    Add request and responce that will help to understand whether a user exists

---
+++ src/UserService.Models.Broker/Requests/IUserExistsAndActiveRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LT.DigitalOffice.Broker.Requests
+{
+    public interface IUserExistsAndActiveRequest
+    {
+        Guid UserId { get; }
+
+        static object CreateObj(Guid userId)
+        {
+            return new
+            {
+                UserId = userId
+            };
+        }
+    }
+}
---
+++ src/UserService.Models.Broker/Responses/IUserExistsAndActiveResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LT.DigitalOffice.Broker.Responses
+{
+    public interface IUserExistsAndActiveResponse
+    {
+        bool IsUserExists { get; }
+        bool? IsActive { get; }
+
+        static object CreateObj(bool isUserExists, bool? isActive)
+        {
+            return new
+            {
+                IsUserExists = isUserExists,
+                IsActive = isActive
+            };
+        }
+    }
+}
