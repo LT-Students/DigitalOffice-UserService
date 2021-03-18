d1cb2b8bb220a86be574220a11747b6b9c39f1d0
Andrey Smyshlyaev <smyshlyaev1104@yandex.ru>
2021-03-18 20:30:48 +0300

    Added GeneratePassword command & interface

---
+++ src/UserService.Business/Interfaces/IGeneratePasswordCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.UserService.Business.Interfaces
+{
+    interface IGeneratePasswordCommand
+    {
+        string Execute();
+    }
+}
