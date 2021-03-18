b171f728040942f48fe5c06d9a6b0bcbc50b604c
Andrey Smyshlyaev <smyshlyaev1104@yandex.ru>
2021-03-19 00:50:16 +0300

    Added GeneratePasswordCommand unit test

---
+++ test/UserService.Business.UnitTests/GeneratePasswordCommandTests.cs
@@ -0,0 +1,33 @@
+﻿using LT.DigitalOffice.UserService.Business.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.UserService.Business.UnitTests
+{
+    public class GeneratePasswordCommandTests
+    {
+        private IGeneratePasswordCommand command;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            command = new GeneratePasswordCommand();
+        }
+
+        [Test]
+        public void TEst()
+        {
+            for (int i = 0; i < 250; i++)
+            {
+                Assert.IsTrue(Regex.IsMatch(command.Execute(), "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@!$_*#]).{8,12}$"));
+
+            }
+        }
+    }
+}
