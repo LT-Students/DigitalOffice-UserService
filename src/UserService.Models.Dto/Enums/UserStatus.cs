3521372ca940ee22844672e05c67f9cccec15882
Mariya <mariyasin@list.ru>
2021-03-18 21:25:02 +0300

    small fix

--- src/UserService.Models.Dto/Enums/UserStatus.cs
+++ src/UserService.Models.Dto/Enums/UserStatus.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace LT.DigitalOffice.UserService.Models.Dto.Enums
 {
     public enum UserStatus
