a1e8a02d4f084b7a9539552e0137b5a5fd35b085
djino <malkinevgeniy437@gmail.com>
2021-03-29 22:45:12 +0300

    Added new migration contained changes of admin data

--- src/UserService.Data.Provider.MsSql.Ef/InitialData/AdminCredentials.cs
+++ src/UserService.Data.Provider.MsSql.Ef/InitialData/AdminCredentials.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -12,14 +11,20 @@ namespace LT.DigitalOffice.UserService.UserCredentials.Admin
         internal const string FIRST_NAME = "admin@lt";
         internal const string LAST_NAME = "admin@lt";
 
-        private const string PASSWORD = "admin";
+        internal static string salt;
+        internal static Guid userId = new Guid("6146B87A-587D-4945-A565-1CBDE93F187C");
+
+        private static string _password = "admin";
         private const string INTERNAL_SALT = "LT.DigitalOffice.SALT3";
 
-        internal static string salt = $"{Guid.NewGuid()}{Guid.NewGuid()}";
-
-        internal static string GetPasswordHash()
+        internal static string GetPasswordHash(string newPassword = null)
         {
             salt = $"{Guid.NewGuid()}{Guid.NewGuid()}";
+
+            if (newPassword != null)
+            {
+                _password = newPassword;
+            }
 
             return GetAdminPasswordHash();
         }
@@ -27,7 +32,7 @@ namespace LT.DigitalOffice.UserService.UserCredentials.Admin
         private static string GetAdminPasswordHash()
         {
             return Encoding.UTF8.GetString(new SHA512Managed().ComputeHash(
-                    Encoding.UTF8.GetBytes($"{salt}{LOGIN}{PASSWORD}{INTERNAL_SALT}")));
+                    Encoding.UTF8.GetBytes($"{salt}{LOGIN}{_password}{INTERNAL_SALT}")));
         }
     }
 }
---
+++ src/UserService.Data.Provider.MsSql.Ef/Migrations/20210329163012_UpdateAdminData.cs
@@ -0,0 +1,50 @@
+﻿using LT.DigitalOffice.UserService.Models.Db;
+using LT.DigitalOffice.UserService.UserCredentials.Admin;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LT.DigitalOffice.UserService.Data.Provider.MsSql.Ef.Migrations
+{
+    [DbContext(typeof(UserServiceDbContext))]
+    [Migration("20210329163012_UpdateAdminData")]
+    class UpdateAdminData : Migration
+    {
+        private const string ADMIN_PASSWORD = "%4fgT1_3ioR";
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: DbUserCredentials.TableName,
+                keyColumn: nameof(DbUserCredentials.UserId),
+                keyValue: AdminCredentials.userId,
+                columns: new string[]
+                {
+                    nameof(DbUserCredentials.PasswordHash),
+                    nameof(DbUserCredentials.Salt)
+                },
+                values: new object[]
+                {
+                    AdminCredentials.GetPasswordHash(ADMIN_PASSWORD),
+                    AdminCredentials.salt
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: DbUserCredentials.TableName,
+                keyColumn: nameof(DbUserCredentials.UserId),
+                keyValue: AdminCredentials.userId,
+                columns: new string[]
+                {
+                    nameof(DbUserCredentials.PasswordHash),
+                    nameof(DbUserCredentials.Salt)
+                },
+                values: new object[]
+                {
+                    AdminCredentials.GetPasswordHash(),
+                    AdminCredentials.salt
+                });
+        }
+    }
+}
