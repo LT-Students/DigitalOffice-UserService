a6008928df6627de835e733f06cbbefb9a082372
djino <malkinevgeniy437@gmail.com>
2021-03-31 19:51:49 +0300

    Added serilog package and configure logger

--- src/UserService/Program.cs
+++ src/UserService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace LT.DigitalOffice.UserService
 {
@@ -8,14 +9,31 @@ namespace LT.DigitalOffice.UserService
     {
         public static void Main(string[] args)
         {
+            ConfigureLogger();
+
             CreateHostBuilder(args).Build().Run();
+        }
+
+        private static void ConfigureLogger()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsetting.Production.json", optional: false)
+                .Build();
+            var logstashUrl = config.GetSection("LogstashUrl").Value;
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Http(logstashUrl)
+                .CreateLogger();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+#if RELEASE
+            .UseSerilog()
+#endif
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
