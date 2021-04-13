e12211832f74147ca726bb4abb5aec66253c4bb4
Evgeniy <malkinevgeniy437@gmail.com>
2021-04-13 12:31:29 +0300

    Add template parsing logic (#42)
    * Add requestClient
    
    * Fixes
    
    * Removed not necessary variables
    
    * reworked tests
    
    * Add tests
    
    * Fix format

---
+++ src/UserService.Models.Broker/Enums/EmailTemplateType.cs
@@ -0,0 +1,9 @@
+﻿namespace LT.DigitalOffice.MessageService.Models.Dto.Enums
+{
+    public enum EmailTemplateType
+    {
+        Notification = 0,
+        Warning = 1,
+        Greeting = 2
+    }
+}
