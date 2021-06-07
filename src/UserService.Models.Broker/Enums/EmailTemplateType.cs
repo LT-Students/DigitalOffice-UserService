1e740f222fefad838ee5720d5e0358c5cae4beea
djino <malkinevgeniy437@gmail.com>
2021-06-07 16:09:30 +0300

    Removed broker models project

--- src/UserService.Models.Broker/Enums/EmailTemplateType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LT.DigitalOffice.MessageService.Models.Dto.Enums
-{
-    public enum EmailTemplateType
-    {
-        Notification = 0,
-        Warning = 1,
-        Greeting = 2
-    }
-}
