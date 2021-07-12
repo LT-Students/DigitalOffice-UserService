a8763233d19edba669012ac70bc4e926aa9a1722
BelehovEgor <61228367+BelehovEgor@users.noreply.github.com>
2021-07-12 16:38:02 +0300

    updated find user endpoint (#72)
    * edit endpoint
    
    * fix

---
+++ src/UserService.Mappers/Models/DepartmentInfoMapper.cs
@@ -0,0 +1,23 @@
+﻿using LT.DigitalOffice.Models.Broker.Models;
+using LT.DigitalOffice.UserService.Mappers.Models.Interfaces;
+using LT.DigitalOffice.UserService.Models.Dto.Models;
+
+namespace LT.DigitalOffice.UserService.Mappers.Models
+{
+    public class DepartmentInfoMapper : IDepartmentInfoMapper
+    {
+        public DepartmentInfo Map(DepartmentData department)
+        {
+            if (department == null)
+            {
+                return null;
+            }
+
+            return new DepartmentInfo
+            {
+                Id = department.Id,
+                Name = department.Name
+            };
+        }
+    }
+}
