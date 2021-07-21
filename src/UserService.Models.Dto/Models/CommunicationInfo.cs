c528f4638fe90f550573fd54ce58d02ccead3d30
Igorok <fdiii@mail.ru>
2021-07-21 12:22:13 +0300

    init

--- src/UserService.Models.Dto/Models/CommunicationInfo.cs
+++ src/UserService.Models.Dto/Models/CommunicationInfo.cs
@@ -4,7 +4,7 @@ namespace LT.DigitalOffice.UserService.Models.Dto.Models
 {
     public record CommunicationInfo
     {
-        public string Type { get; set; }
+        public CommunicationType Type { get; set; }
         public string Value { get; set; }
     }
 }
