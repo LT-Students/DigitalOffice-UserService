5e6fdd5b199743fce5cef0b5e804c700d314e6ef
ndoriomedov <doriomedov213@gmail.com>
2021-08-05 22:07:39 +0300

    add and debug login validator to CreateCredentialsCommand

---
+++ src/UserService.Validation/Login/Interfaces/ILoginValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.Kernel.Attributes;
+
+namespace LT.DigitalOffice.UserService.Validation.Login.Interfaces
+{
+    [AutoInject]
+    public interface ILoginValidator : IValidator<string>
+    {
+
+    }
+}
