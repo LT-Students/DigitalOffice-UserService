f7e72fa966dc4b1dfe43de3979e9612bad117453
Nikita Yurkovsky <cheephot4@gmail.com>
2021-08-12 02:09:00 +0300

    Update  validator and create validator tests

--- src/UserService.Validation/Credentials/CreateCredentialsRequestValidator.cs
+++ src/UserService.Validation/Credentials/CreateCredentialsRequestValidator.cs
@@ -1,11 +1,7 @@
 ﻿using FluentValidation;
 using LT.DigitalOffice.UserService.Models.Dto.Requests.Credentials;
 using LT.DigitalOffice.UserService.Validation.Credentials.Interfaces;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.UserService.Validation.Credentials
 {
@@ -15,26 +11,21 @@ namespace LT.DigitalOffice.UserService.Validation.Credentials
         {
             RuleFor(request => request.Login)
                 .NotEmpty()
-                .WithMessage("Login can't be empty");
+                .WithMessage("Login can't be empty")
+                .Must(login => char.IsLetter(login[0]))
+                .WithMessage("Login must start with letter")
+                .MinimumLength(3)
+                .WithMessage("Login is too short")
+                .MaximumLength(15)
+                .WithMessage("Login is too long")
+                .Must(login => login.All(char.IsLetterOrDigit))
+                .WithMessage("Login must contain only letters or digits");
             RuleFor(request => request.UserId)
                 .NotEmpty()
                 .WithMessage("UserId can't be empty");
             RuleFor(request => request.Password)
                 .NotEmpty()
-                .WithMessage("Password can't be empty"); 
-            When(request => !string.IsNullOrEmpty(request.Login),
-            () =>
-            {
-                RuleFor(request => request.Login)
-                    .Must(login => char.IsLetter(login[0]))
-                    .WithMessage("Login must start with letter")
-                    .MinimumLength(3)
-                    .WithMessage("Login is too short")
-                    .MaximumLength(15)
-                    .WithMessage("Login is too long")
-                    .Must(login => login.All(char.IsLetterOrDigit))
-                    .WithMessage("Login must contain only letters or digits");
-            });
+                .WithMessage("Password can't be empty");
         }
     }
 }
---
+++ test/UserService.Validation.UnitTests/Credentials/CreateCredentialsRequestValidatorTests.cs
@@ -0,0 +1,98 @@
+﻿using FluentValidation.TestHelper;
+using LT.DigitalOffice.UserService.Data.Interfaces;
+using LT.DigitalOffice.UserService.Models.Db;
+using LT.DigitalOffice.UserService.Models.Dto.Requests.Credentials;
+using LT.DigitalOffice.UserService.Models.Dto.Requests.User.Certificates;
+using LT.DigitalOffice.UserService.Validation.Certificates;
+using LT.DigitalOffice.UserService.Validation.Credentials;
+using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+using System;
+
+namespace LT.DigitalOffice.UserService.Validation.UnitTests.Credentials
+{
+    class CreateCredentialsRequestValidatorTests
+    {
+        private CreateCredentialsRequestValidator _validator;
+        private CreateCredentialsRequest _request;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _validator = new CreateCredentialsRequestValidator();
+
+            _request = new CreateCredentialsRequest
+            {
+                Login = "Login1234567890",
+                Password = "Password",
+                UserId = new Guid("A7249129-B47F-490C-8588-C998A76584AB")
+            };
+        }
+
+        [Test]
+        public void ShouldNotThrowAnyException()
+        {
+            _validator.TestValidate(_request).ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenAllIsNotCorrect()
+        {
+            _request = new CreateCredentialsRequest
+            {
+                Login = string.Empty,
+                Password = string.Empty,
+                UserId = Guid.Empty
+            };
+
+            _validator.TestValidate(_request).ShouldHaveValidationErrorFor(x => x.Login);
+            _validator.TestValidate(_request).ShouldHaveValidationErrorFor(x => x.Password);
+            _validator.TestValidate(_request).ShouldHaveValidationErrorFor(x => x.UserId);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenLoginTooShort()
+        {
+            _request = new CreateCredentialsRequest
+            {
+                Login = new string('a', 2)
+            };
+
+            _validator.TestValidate(_request).ShouldHaveValidationErrorFor(x => x.Login);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenLoginTooLong()
+        {
+            _request = new CreateCredentialsRequest
+            {
+                Login = new string('a', 16)
+            };
+
+            _validator.TestValidate(_request).ShouldHaveValidationErrorFor(x => x.Login);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenLoginStartNotInLetter()
+        {
+            _request = new CreateCredentialsRequest
+            {
+                Login = new string("1Login")
+            };
+
+            _validator.TestValidate(_request).ShouldHaveValidationErrorFor(x => x.Login);
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenLoginHaveSpecialCharacters()
+        {
+            _request = new CreateCredentialsRequest
+            {
+                Login = new string("Logi:n")
+            };
+
+            _validator.TestValidate(_request).ShouldHaveValidationErrorFor(x => x.Login);
+        }
+    }
+}
