83e106e74494eeb77ec15151f35866c897461425
Mariya <mariyasin@list.ru>
2021-09-01 10:48:32 +0300

    fixed get user filtres

--- src/UserService.Models.Dto/Requests/User/Filters/GetUserFilter.cs
+++ src/UserService.Models.Dto/Requests/User/Filters/GetUserFilter.cs
@@ -15,48 +15,48 @@ namespace LT.DigitalOffice.UserService.Models.Dto.Requests.User.Filters
         public string Email { get; set; }
 
         [FromQuery(Name = "includecommunications")]
-        public bool? IncludeCommunications { get; set; }
+        public bool IncludeCommunications { get; set; } = false;
 
         [FromQuery(Name = "includecertificates")]
-        public bool? IncludeCertificates { get; set; }
+        public bool IncludeCertificates { get; set; } = false;
 
         [FromQuery(Name = "includeachievements")]
-        public bool? IncludeAchievements { get; set; }
+        public bool IncludeAchievements { get; set; } = false;
 
         [FromQuery(Name = "includedepartment")]
-        public bool? IncludeDepartment { get; set; }
+        public bool IncludeDepartment { get; set; } = false;
 
         [FromQuery(Name = "includeposition")]
-        public bool? IncludePosition { get; set; }
+        public bool IncludePosition { get; set; } = false;
 
         [FromQuery(Name = "includeoffice")]
-        public bool? IncludeOffice { get; set; }
+        public bool IncludeOffice { get; set; } = false;
 
         [FromQuery(Name = "includerole")]
-        public bool? IncludeRole { get; set; }
+        public bool IncludeRole { get; set; } = false;
 
         [FromQuery(Name = "includeskills")]
-        public bool? IncludeSkills { get; set; }
+        public bool IncludeSkills { get; set; } = false;
 
         [FromQuery(Name = "includeeducations")]
-        public bool? IncludeEducations { get; set; }
+        public bool IncludeEducations { get; set; } = false;
 
         [FromQuery(Name = "includeimages")]
-        public bool? IncludeImages { get; set; }
+        public bool IncludeImages { get; set; } = false;
 
         [FromQuery(Name = "includeprojects")]
-        public bool? IncludeProjects { get; set; }
+        public bool IncludeProjects { get; set; } = false;
 
-        public bool IsIncludeDepartment => IncludeDepartment.HasValue && IncludeDepartment.Value;
-        public bool IsIncludePosition => IncludePosition.HasValue && IncludePosition.Value;
-        public bool IsIncludeOffice => IncludeOffice.HasValue && IncludeOffice.Value;
-        public bool IsIncludeRole => IncludeRole.HasValue && IncludeRole.Value;
-        public bool IsIncludeAchievements => IncludeAchievements.HasValue && IncludeAchievements.Value;
-        public bool IsIncludeCertificates => IncludeCertificates.HasValue && IncludeCertificates.Value;
-        public bool IsIncludeCommunications => IncludeCommunications.HasValue && IncludeCommunications.Value;
-        public bool IsIncludeImages => IncludeImages.HasValue && IncludeImages.Value;
-        public bool IsIncludeSkills => IncludeSkills.HasValue && IncludeSkills.Value;
-        public bool IsIncludeEducation => IncludeEducations.HasValue && IncludeEducations.Value;
-        public bool IsIncludeProjects => IncludeProjects.HasValue && IncludeProjects.Value;
+        public bool IsIncludeDepartment => IncludeDepartment;//.HasValue && IncludeDepartment.Value;
+        public bool IsIncludePosition => IncludePosition;//.HasValue && IncludePosition.Value;
+        public bool IsIncludeOffice => IncludeOffice;//.HasValue && IncludeOffice.Value;
+        public bool IsIncludeRole => IncludeRole;//.HasValue && IncludeRole.Value;
+        public bool IsIncludeAchievements => IncludeAchievements;//.HasValue && IncludeAchievements.Value;
+        public bool IsIncludeCertificates => IncludeCertificates;//;.HasValue && IncludeCertificates.Value;
+        public bool IsIncludeCommunications => IncludeCommunications;//.HasValue && IncludeCommunications.Value;
+        public bool IsIncludeImages => IncludeImages;//.HasValue && IncludeImages.Value;
+        public bool IsIncludeSkills => IncludeSkills;//.HasValue && IncludeSkills.Value;
+        public bool IsIncludeEducation => IncludeEducations;//.HasValue && IncludeEducations.Value;
+        public bool IsIncludeProjects => IncludeProjects;//.HasValue && IncludeProjects.Value;*/
     }
 }
