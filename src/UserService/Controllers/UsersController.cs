c7457048fa544711528abca25f32bf190e79cc49
Mariya <63921256+MariyaSin@users.noreply.github.com>
2021-09-02 11:04:00 +0300

    delete user disable endpoint add config (#118)
    * fixed get user filtres
    
    * fix get user filter
    
    * fix
    
    * fix

--- src/UserService/Controllers/UsersController.cs
+++ src/UserService/Controllers/UsersController.cs
@@ -14,72 +14,64 @@ using System.Net;
 
 namespace LT.DigitalOffice.UserService.Controllers
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
+  [Route("[controller]")]
+  [ApiController]
+  public class UsersController : ControllerBase
+  {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    public UsersController(
+      IHttpContextAccessor httpContextAccessor)
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        public UsersController(
-            IHttpContextAccessor httpContextAccessor)
-        {
-            _httpContextAccessor = httpContextAccessor;
-        }
-
-        [HttpPost("create")]
-        public OperationResultResponse<Guid> Create(
-            [FromServices] ICreateUserCommand command,
-            [FromBody] CreateUserRequest request)
-        {
-            var result = command.Execute(request);
-
-            if (result.Status == OperationResultStatusType.Conflict)
-            {
-                _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.Conflict;
-
-                return result;
-            }
-
-            if (result.Status != OperationResultStatusType.Failed)
-            {
-                _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.Created;
-            }
-
-            return result;
-        }
-
-        [HttpPatch("edit")]
-        public OperationResultResponse<bool> Edit(
-            [FromServices] IEditUserCommand command,
-            [FromQuery] Guid userId,
-            [FromBody] JsonPatchDocument<EditUserRequest> request)
-        {
-            return command.Execute(userId, request);
-        }
-
-        [HttpGet("get")]
-        public OperationResultResponse<UserResponse> Get(
-            [FromServices] IGetUserCommand command,
-            [FromQuery] GetUserFilter filter)
-        {
-            return command.Execute(filter);
-        }
-
-        [HttpDelete("disable")]
-        public OperationResultResponse<bool> Disable(
-            [FromServices] IDisableUserCommand command,
-            [FromQuery] Guid userId)
-        {
-            return command.Execute(userId);
-        }
-
-        [HttpGet("find")]
-        public FindResultResponse<UserInfo> Find(
-            [FromServices] IFindUserCommand command,
-            [FromQuery] int skipCount,
-            [FromQuery] int takeCount,
-            [FromQuery] Guid? departmentId)
-        {
-            return command.Execute(skipCount, takeCount, departmentId);
-        }
+      _httpContextAccessor = httpContextAccessor;
     }
+
+    [HttpPost("create")]
+    public OperationResultResponse<Guid> Create(
+      [FromServices] ICreateUserCommand command,
+      [FromBody] CreateUserRequest request)
+    {
+      var result = command.Execute(request);
+
+      if (result.Status == OperationResultStatusType.Conflict)
+      {
+        _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.Conflict;
+
+        return result;
+      }
+
+      if (result.Status != OperationResultStatusType.Failed)
+      {
+        _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.Created;
+      }
+
+      return result;
+    }
+
+    [HttpPatch("edit")]
+    public OperationResultResponse<bool> Edit(
+      [FromServices] IEditUserCommand command,
+      [FromQuery] Guid userId,
+      [FromBody] JsonPatchDocument<EditUserRequest> request)
+    {
+      return command.Execute(userId, request);
+    }
+
+    [HttpGet("get")]
+    public OperationResultResponse<UserResponse> Get(
+      [FromServices] IGetUserCommand command,
+      [FromQuery] GetUserFilter filter)
+    {
+      return command.Execute(filter);
+    }
+
+    [HttpGet("find")]
+    public FindResultResponse<UserInfo> Find(
+      [FromServices] IFindUserCommand command,
+      [FromQuery] int skipCount,
+      [FromQuery] int takeCount,
+      [FromQuery] Guid? departmentId)
+    {
+      return command.Execute(skipCount, takeCount, departmentId);
+    }
+  }
 }
