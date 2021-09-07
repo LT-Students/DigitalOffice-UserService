f8897107aff8a94291c7feaeb65842d13be801bb
Glunky <vkontakte13011997@gmail.com>
2021-09-08 00:13:26 +0300

    added validation

--- src/UserService.Business/Commands/Skill/CreateSkillCommand.cs
+++ src/UserService.Business/Commands/Skill/CreateSkillCommand.cs
@@ -4,6 +4,7 @@ using LT.DigitalOffice.Kernel.Enums;
 using LT.DigitalOffice.Kernel.Extensions;
 using LT.DigitalOffice.Kernel.Responses;
 using LT.DigitalOffice.UserService.Business.Commands.Skill.Interfaces;
+using LT.DigitalOffice.UserService.Validation.Skill.Interfaces;
 using LT.DigitalOffice.UserService.Data.Interfaces;
 using LT.DigitalOffice.UserService.Mappers.Db.Interfaces;
 using LT.DigitalOffice.UserService.Models.Db;
@@ -11,6 +12,8 @@ using LT.DigitalOffice.UserService.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Net;
+using System.Collections.Generic;
+using LT.DigitalOffice.Kernel.FluentValidationExtensions;
 
 namespace LT.DigitalOffice.UserService.Business.Commands.Skill
 {
@@ -21,19 +24,22 @@ namespace LT.DigitalOffice.UserService.Business.Commands.Skill
         private readonly IDbSkillMapper _mapper;
         private readonly ISkillRepository _skillRepository;
         private readonly IAccessValidator _accessValidator;
+        private readonly ICreateSkillRequestValidator _validator;
 
         public CreateSkillCommand(
             IHttpContextAccessor httpContextAccessor,
             IUserRepository userRepository,
             IDbSkillMapper mapper,
             ISkillRepository skillRepository,
-            IAccessValidator accessValidator)
+            IAccessValidator accessValidator,
+            ICreateSkillRequestValidator validator)
         {
             _httpContextAccessor = httpContextAccessor;
             _userRepository = userRepository;
             _mapper = mapper;
             _skillRepository = skillRepository;
             _accessValidator = accessValidator;
+            _validator = validator;
         }
 
         public OperationResultResponse<Guid> Execute(CreateSkillRequest request)
@@ -49,6 +55,15 @@ namespace LT.DigitalOffice.UserService.Business.Commands.Skill
                 _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
 
                 return response;
+            }
+
+            if (!_validator.ValidateCustom(request, out List<string> errors))
+            {
+              _httpContextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+              response.Errors.AddRange(errors);
+              response.Status = OperationResultStatusType.Failed;
+
+              return response;
             }
 
             if (_skillRepository.DoesSkillAlreadyExist(request.Name))
--- src/UserService.Validation/Skill/CreateSkillRequestValidator.cs
+++ src/UserService.Validation/Skill/CreateSkillRequestValidator.cs
@@ -1,9 +1,10 @@
 ﻿using FluentValidation;
 using LT.DigitalOffice.UserService.Models.Dto;
+using LT.DigitalOffice.UserService.Validation.Skill.Interfaces;
 
 namespace LT.DigitalOffice.UserService.Validation.Skill
 {
-    public class CreateSkillRequestValidator : AbstractValidator<CreateSkillRequest>
+    public class CreateSkillRequestValidator : AbstractValidator<CreateSkillRequest>, ICreateSkillRequestValidator
     {
         public CreateSkillRequestValidator()
         {
