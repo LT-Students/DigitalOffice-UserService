2052ca44310a64f19c0812b986a148b1c74ad5c7
BelehovEgor <belehov.egor2001@yandex.ru>
2021-11-02 15:38:38 +0300

    update ver

--- src/UserService/Startup.cs
+++ src/UserService/Startup.cs
@@ -233,7 +233,7 @@ namespace LT.DigitalOffice.UserService
         .GetSection(BaseRabbitMqConfig.SectionName)
         .Get<RabbitMqConfig>();
 
-      Version = "1.4.0.2";
+      Version = "1.4.0.3";
       Description = "UserService is an API that intended to work with users.";
       StartTime = DateTime.UtcNow;
       ApiName = $"LT Digital Office - {_serviceInfoConfig.Name}";
