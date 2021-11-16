84d1491523ec67e400ac2d526c94ce411c8455b0
grinakate <grinakate21@gmail.com>
2021-11-16 10:03:19 +0300

    add DbUserLocation

---
+++ src/UserService.Data.Provider.MsSql.Ef/Migrations/20211115205800_AddUserLocationTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using LT.DigitalOffice.UserService.Models.Db;
+using System;
+
+namespace LT.DigitalOffice.UserService.Data.Provider.MsSql.Ef.Migrations
+{
+  [DbContext(typeof(UserServiceDbContext))]
+  [Migration("20211115205800_AddUserLocationTable")]
+  class AddUserLocationTable : Migration
+  {
+    protected override void Up(MigrationBuilder builder)
+    {
+      builder.CreateTable(
+        name: DbUserLocation.TableName,
+        columns: table => new
+        {
+          Id = table.Column<Guid>(nullable: false),
+          UserId = table.Column<Guid>(nullable: false),
+          Latitude = table.Column<double>(nullable: true),
+          Longitude = table.Column<double>(nullable: true),
+          BusinessHoursFromUtc = table.Column<DateTime>(nullable: true),
+          BusinessHoursToUtc = table.Column<DateTime>(nullable: true),
+          CreatedBy = table.Column<Guid>(nullable: false),
+          CreatedAtUtc = table.Column<DateTime>(nullable: false),
+          ModifiedBy = table.Column<Guid>(nullable: true),
+          ModifiedAtUtc = table.Column<DateTime>(nullable: true)
+        },
+        constraints: table =>
+        {
+          table.PrimaryKey("PK_UserLocationTable", x => x.Id);
+        });
+    }
+  }
+}
---
+++ src/UserService.Models.Db/DbUserLocation.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace LT.DigitalOffice.UserService.Models.Db
+{
+  public class DbUserLocation
+  {
+    public const string TableName = "UserLocations";
+
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+    public DateTime? BusinessHoursFromUtc { get; set; }
+    public DateTime? BusinessHoursToUtc { get; set; }
+    public Guid CreatedBy { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public Guid? ModifiedBy { get; set; }
+    public DateTime? ModifiedAtUtc { get; set; }
+
+    public DbUser User { get; set; }
+  }
+
+  public class DbUserLocationConfiguration : IEntityTypeConfiguration<DbUserLocation>
+  {
+    public void Configure(EntityTypeBuilder<DbUserLocation> builder)
+    {
+      builder
+        .ToTable(DbUserLocation.TableName);
+
+      builder
+        .HasKey(x => x.Id);
+
+      builder
+        .HasOne(l => l.User)
+        .WithOne(ul => ul.Location);
+    }
+  }
+}
