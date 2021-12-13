69a4eb882f72cbf772f82c3f7d1b145a1a2b863a
VladSpitsin <63400560+VladSpitsin@users.noreply.github.com>
2021-12-13 21:49:50 +0300

    add CreateGender endpoint #2 (#166)
    * add CreateGender endpoint
    
    * fix .idea
    
    * fix
    
    * fixes
    
    * fix rights
    * responseCreator
    * fix validator
    * fix names
    * fix ignore-case
    
    * fixes
    
    * fix request
    * fix rights check
    * fix async
    
    * fixes
    
    * fix async
    * fix validator
    * remove extra return
    
    * fixes
    
    * update specification version
    * fix validator
    
    * fix trim

---
+++ src/UserService/Controllers/GenderController.cs
@@ -0,0 +1,22 @@
+﻿using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.UserService.Business.Commands.User.Interfaces;
+using LT.DigitalOffice.UserService.Models.Dto.Requests.User;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.UserService.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class GenderController : ControllerBase
+  {
+    [HttpPost("create")]
+    public async Task<OperationResultResponse<Guid?>> CreateAsync(
+      [FromServices] ICreateGenderCommand command,
+      [FromBody] CreateGenderRequest request)
+    {
+      return await command.ExecuteAsync(request);
+    }
+  }
+}
