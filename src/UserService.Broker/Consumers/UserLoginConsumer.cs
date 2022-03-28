fea7e9ae6947d52f826598e5dd7a7a91a84ce72b
Mariya <63921256+MariyaSin@users.noreply.github.com>
2022-03-28 19:10:26 +0300

    added login to ForgotPasswordCommand (#191)
    * added login to ForgotPasswordCommand
    added CredentialsParser
    Fixed login by email
    
    * fix

--- src/UserService.Broker/Consumers/UserLoginConsumer.cs
+++ src/UserService.Broker/Consumers/UserLoginConsumer.cs
@@ -2,13 +2,11 @@
 using LT.DigitalOffice.Kernel.Exceptions.Models;
 using LT.DigitalOffice.Models.Broker.Requests.User;
 using LT.DigitalOffice.Models.Broker.Responses.User;
+using LT.DigitalOffice.UserService.Broker.Helpers.Login;
 using LT.DigitalOffice.UserService.Data.Interfaces;
 using LT.DigitalOffice.UserService.Models.Db;
 using LT.DigitalOffice.UserService.Models.Dto.Requests.Credentials.Filters;
 using MassTransit;
-using System;
-using System.Net.Mail;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.UserService.Broker.Consumers
@@ -21,11 +19,11 @@ namespace LT.DigitalOffice.UserService.Broker.Consumers
     {
       GetCredentialsFilter result = new();
 
-      if (IsEmail(request.LoginData))
+      if (request.LoginData.IsEmail())
       {
         result.Email = request.LoginData;
       }
-      else if (IsPhone(request.LoginData))
+      else if (request.LoginData.IsPhone())
       {
         result.Phone = request.LoginData;
       }
@@ -55,41 +53,6 @@ namespace LT.DigitalOffice.UserService.Broker.Consumers
         dbUserCredentials.PasswordHash,
         dbUserCredentials.Salt,
         dbUserCredentials.Login);
-    }
-
-    private bool IsEmail(string value)
-    {
-      try
-      {
-        var m = new MailAddress(value);
-
-        return true;
-      }
-      catch (FormatException)
-      {
-        return false;
-      }
-    }
-
-    private bool IsPhone(string value)
-    {
-      StringBuilder sb = new();
-
-      if (value.IndexOf("+") == 0)
-      {
-        sb.Append(value[0]);
-      }
-
-      foreach (char c in value)
-      {
-        if (!char.IsNumber(c))
-        {
-          continue;
-        }
-        sb.Append(c);
-      }
-
-      return sb.Length == value.Length;
     }
 
     public UserLoginConsumer(
---
+++ src/UserService.Broker/Helpers/Login/CredentialsParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Mail;
+using System.Text;
+
+namespace LT.DigitalOffice.UserService.Broker.Helpers.Login;
+public static class CredentialsParser
+{
+  public static bool IsEmail(this string value)
+  {
+    try
+    {
+      MailAddress m = new MailAddress(value);
+
+      return true;
+    }
+    catch (FormatException)
+    {
+      return false;
+    }
+  }
+
+  public static bool IsPhone(this string value)
+  {
+    StringBuilder sb = new();
+
+    foreach (char c in value)
+    {
+      if (!char.IsNumber(c))
+      {
+        continue;
+      }
+      sb.Append(c);
+    }
+
+    return sb.Length == value.Length;
+  }
+}
