b7059fc5d8e6ac870278c55761e76606df21f7c1
Mariya <63921256+MariyaSin@users.noreply.github.com>
2022-04-04 19:13:56 +0300

    fix edit active (#193)

--- src/UserService.Validation/User/EditUserActiveRequestValidator.cs
+++ src/UserService.Validation/User/EditUserActiveRequestValidator.cs
@@ -1,24 +1,36 @@
 ﻿using FluentValidation;
 using LT.DigitalOffice.UserService.Data.Interfaces;
+using LT.DigitalOffice.UserService.Models.Db;
+using LT.DigitalOffice.UserService.Models.Dto.Enums;
 using LT.DigitalOffice.UserService.Models.Dto.Requests.User;
 using LT.DigitalOffice.UserService.Validation.User.Interfaces;
+using System.Linq;
 
 namespace LT.DigitalOffice.UserService.Validation.User
 {
-  public class EditUserActiveRequestValidator : AbstractValidator<EditUserActiveRequest>, IEditUserActiveRequestValidator
+  public class EditUserActiveRequestValidator : AbstractValidator<(DbUser dbUser, EditUserActiveRequest request)>, IEditUserActiveRequestValidator
   {
     public EditUserActiveRequestValidator(
-      IUserRepository _userRepository,
       IPendingUserRepository _pendingRepository)
     {
-      RuleFor(request => request)
+      RuleFor(x => x)
         .Cascade(CascadeMode.Stop)
-        .MustAsync(async (request, _) =>
-          (await _userRepository.GetAsync(request.UserId))?.IsActive != request.IsActive)
+        .Must(x => x.dbUser.IsActive != x.request.IsActive)
         .WithMessage("Error is active value.")
-        .MustAsync(async (request, _) =>
-          !(request.IsActive && await _pendingRepository.GetAsync(request.UserId) is not null))
-        .WithMessage("User already pending.");
+        .MustAsync(async (x, _) =>
+          !(x.request.IsActive && await _pendingRepository.GetAsync(x.request.UserId) is not null))
+        .WithMessage("User already pending.")
+        .Must(x =>
+        {
+          if (x.request.IsActive && x.request.CommunicationId.HasValue)
+          {
+            DbUserCommunication uc = x.dbUser.Communications.FirstOrDefault(c => c.Id == x.request.CommunicationId);
+            return uc is not null && (uc.Type == (int)CommunicationType.Email || uc.Type == (int)CommunicationType.BaseEmail);
+          }
+
+          return !x.request.IsActive;
+        })
+        .WithMessage("Wrong user communication.");
     }
   }
 }
