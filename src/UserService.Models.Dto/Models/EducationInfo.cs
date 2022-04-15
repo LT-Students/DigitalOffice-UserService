6e18a5da0da0d0c0c9e88fb4bcc41cd156469885
Ryzhenkova Ekaterina <93452892+actimellka@users.noreply.github.com>
2022-04-15 11:44:24 +0300

    edit EducationInfoMappera, EducationInfo, add EducationFormInfo and TypeInfo (#196)
    * add EducationFormInfo, EducationTypeInfo, EducationInfo, edit EducationInfoMapper
    
    * fix "C" in Completeness
    
    * fix C
    
    * fix C
    
    * fix "C" in Completeness
    
    * fix

--- src/UserService.Models.Dto/Models/EducationInfo.cs
+++ src/UserService.Models.Dto/Models/EducationInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LT.DigitalOffice.UserService.Models.Dto.Models
 {
@@ -7,8 +8,11 @@ namespace LT.DigitalOffice.UserService.Models.Dto.Models
     public Guid Id { get; set; }
     public string UniversityName { get; set; }
     public string QualificationName { get; set; }
-    public string FormEducation { get; set; }
+    public EducationFormInfo EducationForm { get; set; }
+    public EducationTypeInfo EducationType { get; set; }
+    public string Completeness { get; set; }
     public DateTime AdmissionAt { get; set; }
     public DateTime? IssueAt { get; set; }
+    public List<Guid> ImagesIds { get; set; } 
   }
 }
