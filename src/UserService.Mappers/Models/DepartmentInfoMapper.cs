7ba3ec97f1a483249125d003577de48de7749810
Vladimir Liamin <LyaminVA@yandex.ru>
2022-06-02 15:03:52 +0300

    add role check

--- src/UserService.Mappers/Models/DepartmentInfoMapper.cs
+++ src/UserService.Mappers/Models/DepartmentInfoMapper.cs
@@ -1,11 +1,22 @@
-﻿using LT.DigitalOffice.Models.Broker.Models.Department;
+﻿using LT.DigitalOffice.Kernel.Extensions;
+using LT.DigitalOffice.Models.Broker.Enums;
+using LT.DigitalOffice.Models.Broker.Models.Department;
 using LT.DigitalOffice.UserService.Mappers.Models.Interfaces;
 using LT.DigitalOffice.UserService.Models.Dto.Models;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
 
 namespace LT.DigitalOffice.UserService.Mappers.Models
 {
   public class DepartmentInfoMapper : IDepartmentInfoMapper
   {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public DepartmentInfoMapper(IHttpContextAccessor httpContextAccessor)
+    {
+      _httpContextAccessor = httpContextAccessor;
+    }
+
     public DepartmentInfo Map(DepartmentData department)
     {
       return department is null
@@ -14,7 +25,10 @@ namespace LT.DigitalOffice.UserService.Mappers.Models
         {
           Id = department.Id,
           Name = department.Name,
-          ShortName = department.ShortName
+          ShortName = department.ShortName,
+          ProjectsIds = department.Users.FirstOrDefault(user => (user.UserId == _httpContextAccessor.HttpContext.GetUserId()) && (user.Role == DepartmentUserRole.Manager)) != null
+            ? department.ProjectsIds 
+            : null
         };
     }
   }
