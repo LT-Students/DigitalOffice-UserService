9b832cbe49fa97e91a553b10ad5682d94ba3703d
SolovyovDmitry <101804724+SolovyovDmitry@users.noreply.github.com>
2022-06-16 17:26:17 +0300

    update models version (#212)

---
+++ src/UserService.Broker/Publishes/Interfaces/IPublish.cs
@@ -0,0 +1,13 @@
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.UserService.Broker.Publishes.Interfaces
+{
+  [AutoInject]
+  public interface IPublish
+  {
+    Task RemoveImagesAsync(List<Guid> imagesIds);
+  }
+}
---
+++ src/UserService.Broker/Publishes/Publish.cs
@@ -0,0 +1,28 @@
+﻿using LT.DigitalOffice.Models.Broker.Enums;
+using LT.DigitalOffice.Models.Broker.Publishing.Subscriber.Image;
+using LT.DigitalOffice.UserService.Broker.Publishes.Interfaces;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.UserService.Broker.Publishes
+{
+  public class Publish : IPublish
+  {
+    private readonly IBus _bus;
+
+    public Publish(
+      IBus bus)
+    {
+      _bus = bus;
+    }
+
+    public async Task RemoveImagesAsync(List<Guid> imagesIds)
+    {
+      await _bus.Publish<IRemoveImagesPublish>(IRemoveImagesPublish.CreateObj(
+        imagesIds: imagesIds,
+        imageSource: ImageSource.User));
+    }
+  }
+}
