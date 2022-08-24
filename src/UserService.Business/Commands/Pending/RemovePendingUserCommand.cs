66f298eba11e46029b88d43d4356b7f5380be569
ndoriomedov <doriomedov213@gmail.com>
2022-08-25 02:04:12 +0300

    fix RemovePendingUserCommand

--- src/UserService.Business/Commands/Pending/RemovePendingUserCommand.cs
+++ src/UserService.Business/Commands/Pending/RemovePendingUserCommand.cs
@@ -1,7 +1,9 @@
 ﻿using LT.DigitalOffice.Kernel.BrokerSupport.AccessValidatorEngine.Interfaces;
 using LT.DigitalOffice.Kernel.Constants;
 using LT.DigitalOffice.Kernel.Helpers.Interfaces;
+using LT.DigitalOffice.Kernel.RedisSupport.Helpers.Interfaces;
 using LT.DigitalOffice.Kernel.Responses;
+using LT.DigitalOffice.UserService.Broker.Publishes.Interfaces;
 using LT.DigitalOffice.UserService.Business.Commands.Pending.Interfaces;
 using LT.DigitalOffice.UserService.Data.Interfaces;
 using System;
@@ -15,15 +17,21 @@ namespace LT.DigitalOffice.UserService.Business.Commands.Pending
     private readonly IAccessValidator _accessValidator;
     private readonly IResponseCreator _responseCreator;
     private readonly IPendingUserRepository _repository;
+    private readonly IGlobalCacheRepository _globalCache;
+    private readonly IPublish _publish;
 
     public RemovePendingUserCommand(
       IAccessValidator accessValidator,
       IResponseCreator responseCreator,
-      IPendingUserRepository repository)
+      IPendingUserRepository repository,
+      IGlobalCacheRepository globalCache,
+      IPublish publish)
     {
       _accessValidator = accessValidator;
       _responseCreator = responseCreator;
       _repository = repository;
+      _publish = publish;
+      _globalCache = globalCache;
     }
 
     public async Task<OperationResultResponse<bool>> ExecuteAsync(Guid userId)
@@ -35,6 +43,13 @@ namespace LT.DigitalOffice.UserService.Business.Commands.Pending
 
       OperationResultResponse<bool> response = new();
       response.Body = (await _repository.RemoveAsync(userId)) is not null;
+
+      if (response.Body)
+      {
+        await _publish.DisactivateUserAsync(userId);
+
+        await _globalCache.RemoveAsync(userId);
+      }
 
       return response.Body
         ? response
