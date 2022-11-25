a297dd12034d339357311833d49147a7df599679
vorobeva_marina <92924355+vorobevamv@users.noreply.github.com>
2022-11-25 15:00:18 +0300

    added resx to EditCommunicationRequestValidator, fixed english in message (#257)
    Co-authored-by: sparrowofnight@gmail.com <sparrowofnight@gmail.com>

--- src/UserService.Validation/Communication/EditCommunicationRequestValidator.cs
+++ src/UserService.Validation/Communication/EditCommunicationRequestValidator.cs
@@ -4,6 +4,9 @@ using LT.DigitalOffice.UserService.Models.Db;
 using LT.DigitalOffice.UserService.Models.Dto.Enums;
 using LT.DigitalOffice.UserService.Models.Dto.Requests.Communication;
 using LT.DigitalOffice.UserService.Validation.Communication.Interfaces;
+using LT.DigitalOffice.UserService.Validation.Communication.Resources;
+using System.Globalization;
+using System.Threading;
 
 namespace LT.DigitalOffice.UserService.Validation.Communication
 {
@@ -19,28 +22,30 @@ namespace LT.DigitalOffice.UserService.Validation.Communication
     {
       _communicationRepository = communicationRepository;
 
+      Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+
       RuleFor(x => x)
         .Must(x => !(x.request.Type is not null && !string.IsNullOrEmpty(x.request.Value)))
-        .WithMessage("it is not allowed to change more than 2 properties at the same time.")
+        .WithMessage(EditCommunicationRequestValidatorResource.MoreThenOneProperty)
         .Must(x => !(x.request.Type is null && string.IsNullOrEmpty(x.request.Value)))
-        .WithMessage("No change to apply.");
+        .WithMessage(EditCommunicationRequestValidatorResource.NoChanges);
 
       When(x => x.request.Type is not null, () =>
       {
         RuleFor(x => x)
           .Cascade(CascadeMode.Stop)
           .Must(x => x.request.Type == CommunicationType.BaseEmail)
-          .WithMessage("Invalid type")
+          .WithMessage(EditCommunicationRequestValidatorResource.InvalidType)
           .Must(x => x.dbUserCommunication.IsConfirmed
             && x.dbUserCommunication.Type == (int)CommunicationType.Email)
-          .WithMessage("Only a verified email can be set as the base email.");
+          .WithMessage(EditCommunicationRequestValidatorResource.NotVerifiedEmail);
       });
 
       When(x => !string.IsNullOrEmpty(x.request.Value), () =>
       {
         RuleFor(x => x.request.Value)
           .MustAsync(async (x, _) => !await _communicationRepository.DoesValueExist(x))
-          .WithMessage("Communication value already exist.");
+          .WithMessage(EditCommunicationRequestValidatorResource.ExistingCommunicationValue);
       });
     }
   }
